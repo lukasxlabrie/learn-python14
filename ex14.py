4736b47d2797c02c3a5048dfc4b835028a6d0452
misosucky <126988962+Misosucky@users.noreply.github.com>
2023-09-11 21:44:49 -0400

    fixed note

--- ex14.py
+++ ex14.py
@@ -1,7 +1,7 @@
 #imports to add variable to script. stores var for later use. AKA Modules
 from sys import argv
 
-#defines prompt as > and tells python user_name will be used in script and that sciprt is python
+#defines prompt as > and tells python to take user_name from the user when asking terminal to run python
 scirpt, user_name = argv
 prompt = '> '
 
