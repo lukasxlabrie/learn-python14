4b1b98be5545cbf446de3b235638dcb5a95b061c
misosucky <126988962+Misosucky@users.noreply.github.com>
2023-09-12 18:53:24 -0400

    added , and "

--- ex14.py
+++ ex14.py
@@ -6,7 +6,7 @@ scirpt, user_name = argv
 prompt = '> '
 
 #multple f strings, will prompt for "likes"
-print(f"Hi {user_name}, I'm the {script} script.")
+print(f"Hi", {user_name}, "I'm the", {script},"script.")
 print("I'd like to ask you a few questions.")
 print(f"Do you like me", {user_name},"?")
 likes = input(prompt)
