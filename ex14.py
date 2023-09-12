3ac88b72772f826ea99de5d5df86578aaf0e1809
misosucky <126988962+Misosucky@users.noreply.github.com>
2023-09-11 21:43:34 -0400

    added notes

--- ex14.py
+++ ex14.py
@@ -1,19 +1,25 @@
+#imports to add variable to script. stores var for later use. AKA Modules
 from sys import argv
 
+#defines prompt as > and tells python user_name will be used in script and that sciprt is python
 scirpt, user_name = argv
 prompt = '> '
 
+#multple f strings, will prompt for "likes"
 print(f"Hi {user_name}, I'm the {script} script.")
 print("I'd like to ask you a few questions.")
 print(f"Do you like {user_name}?")
 likes = input(prompt)
 
+#f string and prompts for input "likes"
 print(f"Where do you live {user_name}?")
 lives = input(prompt)
 
+#asks user to enter type of CPU
 print("What kind of a computer do you have?")
 computer = input(prompt)
 
+#final f string. combines all user inputs into one print. """ allows for each line to print as typed.
 print(f"""
 Alright, so you said {likes} about liking me.
 Yoo live in {lives}. Not sure where that is.
